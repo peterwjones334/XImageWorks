30a0bddaa0a30943361b297b986e486d96e7979f
Peter Jones <peter.jones334@btinternet.com>
2024-07-11 17:26:21 +0100

    Change preview to slider

---
+++ ximageapp_0.2.py
@@ -0,0 +1,60 @@
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory
+from werkzeug.utils import secure_filename
+import os
+from PIL import Image
+import effects_aggregator
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+app.config['PROCESSED_FOLDER'] = 'processed/'
+app.config['ALLOWED_EXTENSIONS'] = {'jpeg', 'jpg', 'png'}
+
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
+if not os.path.exists(app.config['PROCESSED_FOLDER']):
+    os.makedirs(app.config['PROCESSED_FOLDER'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            input_image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(input_image_path)
+            
+            # Process the image
+            image = Image.open(input_image_path)
+            selected_effect = request.form.get('effect')
+            
+            if selected_effect in effects_aggregator.effects:
+                image = effects_aggregator.effects[selected_effect](image)
+            
+            output_image_path = os.path.join(app.config['PROCESSED_FOLDER'], filename)
+            image.save(output_image_path)
+            return redirect(url_for('compare_images', filename=filename))
+    return render_template('upload.html', effects=effects_aggregator.effects.keys())
+
+@app.route('/compare/<filename>')
+def compare_images(filename):
+    return render_template('compare.html', filename=filename)
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/processed/<filename>')
+def processed_file(filename):
+    return send_from_directory(app.config['PROCESSED_FOLDER'], filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
